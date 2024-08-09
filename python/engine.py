d79e5fb43b88fde216168851cc40934b70f02561
Pisut Prayoonthamrongthiti <pp76@st-andrews.ac.uk>
2024-08-09 14:19:50 +0100

    add compare test

--- python/engine.py
+++ python/engine.py
@@ -1,109 +1,201 @@
-#a card is a tuple of rank and suit
-#rank goes from 2 to 13 with 13 being Ace
-#suit being c d h s (2, 'c')
 import re
-#pip install more_itertools
+
+
+### need to re sort the all the pairs
 class Hand:
-    rank_dict = {'a':14, 'k':13, 'q':12, 'j':11}
-    handStength = -1 #8 for SF 1 is highcard
-        
 
     def __init__(self, hand_string):
         self.card_list = []
         self.read_handstring(hand_string)
-    
+        self.hand_strength = -1  # 8 for SF 1 is highcard
+        self.assign_hand_strength()
+
     def read_handstring(self, hand_string):
-        suit_pattern = r'[cdhs]'
-        non_transformed= [item for item in re.split(self.suit_pattern, hand_string) if item]
+        rank_dict = {"a": 14, "k": 13, "q": 12, "j": 11}
+        suit_pattern = r"[cdhs]"
+        non_transformed = [item for item in re.split(suit_pattern, hand_string) if item]
         rank_list = []
         for char in non_transformed:
-            if char in self.rank_dict:
-                rank_list.append(self.rank_dict[char])
+            if char in rank_dict:
+                rank_list.append(rank_dict[char])
             else:
                 rank_list.append(int(char))
-        
+
         for rank in rank_list:
             if len(rank_list) != 5:
                 raise ValueError("Wrong number of card rank")
             if rank < 2 or rank > 14:
                 raise ValueError("Invalid card rank " + str(rank))
-        #get the suit
-        
+        # get the suit
+
         suit_list = []
         for char in hand_string:
-            if char in ['c', 'd', 'h', 's']:
+            if char in ["c", "d", "h", "s"]:
                 suit_list.append(char)
         if len(suit_list) != 5:
-            raise ValueError('Invalid number of card suits')
-        
+            raise ValueError("Invalid number of card suits")
+
         self.card_list = list(zip(rank_list, suit_list))
-        
+
         self.card_list.sort(key=lambda x: x[0])
-        
+
+    def assign_hand_strength(self):
+        hand_checks = [
+            (self.check_straight_flush, 8),  # Straight Flush
+            (self.check_four_of_kind, 7),  # Four of a Kind
+            (self.check_full_house, 6),  # Full House
+            (self.check_flush, 5),  # Flush
+            (self.check_straight, 4),  # Straight
+            (self.check_three_of_kind, 3),  # Three of a Kind
+            (self.check_two_pair, 2),  # Two Pair
+            (self.check_one_pair, 1),  # One Pair
+        ]
+
+        for check_function, strength in hand_checks:
+            if check_function():
+                self.hand_strength = strength
+                return
+
+        self.hand_strength = 0  # High Card if no other hand is matched
+
+    def compare(self, another):
+        if not isinstance(another, Hand):
+            raise TypeError("Not a Hand cannot be compared")
+        if self.hand_strength > another.hand_strength:
+            return 1
+        elif self.hand_strength < another.hand_strength:
+            return -1
+        else:
+            # compare the rank of each card one by one
+            for i in range(5):
+                if self.card_list[i][0] > another.card_list[i][0]:
+                    return 1
+                elif self.card_list[i][0] < another.card_list[i][0]:
+                    return -1
+            return 0
+
     def check_flush(self):
-        # Check if all suits are the same
-        suits = [t[1] for t in self.card_list]
-        return all(suit == suits[0] for suit in suits)
+        return all(card[1] == self.card_list[0][1] for card in self.card_list)
 
     def check_straight(self):
-        # Check if the hand is a wheel (special case of straight)
         if self.check_wheel():
             return True
-        
-        # Check if ranks are consecutive
-        return all(self.card_list[i][0] + 1 == self.card_list[i + 1][0] for i in range(len(self.card_list) - 1))
-    
+        return all(
+            self.card_list[i][0] + 1 == self.card_list[i + 1][0]
+            for i in range(len(self.card_list) - 1)
+        )
+
     def check_wheel(self):
-        if self.card_list[4][0] == 14:
-            temp = self.card_list[:-1]
-            temp_rank = [t[0] for t in temp]
-            if temp_rank == [2, 3, 4, 5]:
-                ace_card = self.card_list[4]
-                self.card_list = temp + [ace_card]
-                return True
+        first_four = self.card_list[:-1]
+        first_four_rank = [t[0] for t in first_four]
+        if first_four_rank == [2, 3, 4, 5] and self.card_list[4][0] == 14:
+            temp = self.card_list[4:] + first_four
+            self.card_list = temp
+            return True
         return False
-    
+
     def check_straight_flush(self):
         return self.check_flush() and self.check_straight()
-    
 
-    
     def check_four_of_kind(self):
-        windowed_list = [self.card_list[i:i+4] for i in range(len(self.card_list) - 4 + 1)]
+        four_kind = None
+        windowed_list = [
+            self.card_list[i : i + 4] for i in range(len(self.card_list) - 4 + 1)
+        ]
         for window in windowed_list:
             if all([x[0] == window[0][0] for x in window]):
-                print(window)
-                return True
-        print(windowed_list)
+                four_kind = window
+        if four_kind:
+            temp_list = []
+            temp_list += four_kind
+            for card in self.card_list:
+                if card not in temp_list:
+                    temp_list += [card]
+            self.card_list = temp_list
+            return True
         return False
-    
+
     def check_full_house(self):
-        first_three = self.card_list[:3]
-        last_two = self.card_list[3:]
-        return all(x[0] == first_three[0][0] for x in first_three) and all(y[0] == last_two[0][0] for y in last_two)
-    
+        rank_counts = {}  # Dictionary to count occurrences of each rank
+
+        # Count occurrences of each rank
+        for card in self.card_list:
+            rank = card[0]
+            rank_counts[rank] = rank_counts.get(rank, 0) + 1
+
+        three_kind = None
+        pair = None
+
+        # Identify three of a kind and pair
+        for rank, count in rank_counts.items():
+            if count == 3:
+                three_kind = [card for card in self.card_list if card[0] == rank]
+            elif count == 2:
+                pair = [card for card in self.card_list if card[0] == rank]
+
+        # Check if both three of a kind and a pair are found
+        if three_kind and pair:
+            self.card_list = three_kind + pair
+            return True
+
+        return False
+
     def check_three_of_kind(self):
-        windowed_list = [self.card_list[i:i+3] for i in range(len(self.card_list) - 3 + 1)]
+        three_kind = None
+        windowed_list = [
+            self.card_list[i : i + 3] for i in range(len(self.card_list) - 3 + 1)
+        ]
         for window in windowed_list:
             if all([x[0] == window[0][0] for x in window]):
-                print(window)
-                return True
-        print(windowed_list)
+                three_kind = window
+        if three_kind:
+            temp_list = []
+            temp_list += three_kind
+            for card in self.card_list:
+                if card not in temp_list:
+                    temp_list += card
+            return True
         return False
 
     def check_two_pair(self):
-        windowed_list = [self.card_list[i:i+2] for i in range(len(self.card_list)-2+1)]
-        pair_count = 0
+        first_pair = None
+        second_pair = None
+        windowed_list = [
+            self.card_list[i : i + 2] for i in range(len(self.card_list) - 2 + 1)
+        ]
         for window in windowed_list:
             if all([x[0] == window[0][0] for x in window]):
-                pair_count += 1
-            if pair_count == 2:
-                    return True
+                if not first_pair:
+                    first_pair = window
+                elif first_pair and not second_pair:
+                    second_pair = window
+        if first_pair and second_pair:
+            temp_list = first_pair + second_pair
+            for card in self.card_list:
+                if card not in temp_list:
+                    temp_list += card
+            return True
         return False
-    
+
     def check_one_pair(self):
-        windowed_list = [self.card_list[i:i+1] for i in range(len(self.card_list)-1+1)]
+        pair = None
+        windowed_list = [
+            self.card_list[i : i + 2] for i in range(len(self.card_list) - 1)
+        ]
         for window in windowed_list:
             if all([x[0] == window[0][0] for x in window]):
-                return True
+                pair = window
+        if pair:
+            tempList = pair
+            for card in self.card_list:
+                if card not in tempList:
+                    tempList += card
+            return True
         return False
+
+    def print(self):
+        print("printing")
+        print(self.card_list)
+
+    
+    
--- python/test.py
+++ python/test.py
@@ -5,44 +5,172 @@ from engine import Hand
 class TestHandMethods(unittest.TestCase):
     def setUp(self):
         pass
-    
-    def test_wheel(self):
-        hand = Hand('ad3c2s5d4h')
-        self.assertTrue(hand.check_wheel())
-    
-    def test_not_wheel(self):
-        hand = Hand('ad3c2s7d4h')
-        self.assertFalse(hand.check_wheel())
 
-    def test_check_straight(self):
-        hand = Hand('7d6h5s4c3d')
-        self.assertTrue(hand.check_straight())
-        hand2 = Hand('qcjh10s9h8d')
-        self.assertTrue(hand2.check_straight())
-        hand3 = Hand('ah4d5s3c2h')
-        self.assertTrue(hand3.check_straight())
-    
-    def test_check_straight_flush(self):
-        hand = Hand('ad2d3d4d5d')
-        self.assertTrue(hand.check_straight_flush())
-        
-    def test_four_of_kind(self):
-        hand = Hand('4s4hjs4c4d')
-        self.assertTrue(hand.check_four_of_kind())
-        not_four = Hand('ad2d3d4d5d')
-        self.assertFalse(not_four.check_four_of_kind())
+    def test_straight_with_low_hand(self):
+        hand = Hand("7d6h5s4c3d")
+        self.assertEqual(hand.hand_strength, 4)
 
+    def test_straight_with_high_hand(self):
+        hand2 = Hand("qcjh10s9h8d")
+        self.assertEqual(hand2.hand_strength, 4)
+
+    def test_straight_with_ace_low_hand(self):
+        hand3 = Hand("ah4d5s3c2h")
+        self.assertEqual(hand3.hand_strength, 4)
+
+    # Straight Flush Test
+    def test_straight_flush(self):
+        hand = Hand("ad2d3d4d5d")
+        self.assertEqual(hand.hand_strength, 8)
+
+    # Four of a Kind Tests
+    def test_four_of_a_kind(self):
+        hand = Hand("4s4hjs4c4d")
+        self.assertEqual(hand.hand_strength, 7)
+
+    def test_not_four_of_a_kind(self):
+        not_four = Hand("ad2d3d4d5d")
+        self.assertNotEqual(not_four.hand_strength, 7)
+
+    # Full House Test
     def test_full_house(self):
-        hand = Hand('2s2c2h5h5d')
-        self.assertTrue(hand.check_full_house())
-        
+        hand = Hand("2s2c2h5h5d")
+        self.assertEqual(hand.hand_strength, 6)
+
+    # Two Pair Tests
     def test_two_pair(self):
-        hand = Hand('2c2s5hkhkd')
-        self.assertTrue(hand.check_two_pair())
-        hand2 = Hand('5sks5dkdqs')
-        self.assertTrue(hand.check_two_pair())
-        not_hand = Hand('2s5c7s8dkh')
-        self.assertFalse(hand.check_two_pair())
-    
-if __name__ == '__main__':
+        hand = Hand("2c2s5hkhkd")
+        self.assertEqual(hand.hand_strength, 2)
+
+    def test_two_pair_with_face_cards(self):
+        hand2 = Hand("5sks5dkdqs")
+        self.assertEqual(hand2.hand_strength, 2)
+
+    def test_not_two_pair(self):
+        not_hand = Hand("2s5c7s8dkh")
+        self.assertNotEqual(not_hand.hand_strength, 2)
+
+    # One Pair Test
+    def test_one_pair(self):
+        hand = Hand("3c3d5d7sad")
+        self.assertEqual(hand.hand_strength, 1)
+
+    # Three of a Kind Tests
+    def test_three_of_a_kind(self):
+        hand = Hand("8s8d8h3c2d")
+        self.assertEqual(hand.hand_strength, 3)
+
+    def test_not_three_of_a_kind(self):
+        not_hand = Hand("7c7d5h5s2s")
+        self.assertNotEqual(not_hand.hand_strength, 3)
+
+    # High Card Tests
+    def test_high_card_with_ace(self):
+        hand = Hand("ahksqdjc9s")
+        self.assertEqual(hand.hand_strength, 0)  # Assuming high card is the lowest rank with strength 0
+
+    def test_high_card_with_low_cards(self):
+        hand = Hand("2c4h6s8dkd")
+        self.assertEqual(hand.hand_strength, 0)  # Assuming high card is the lowest rank with strength 0
+        
+    def test_compare_high_card_equal(self):
+        hand1 = Hand("ahkdqcjd9s")  # High card: Ace, King, Queen, Jack, 9
+        hand2 = Hand("adkhqcjs9d")  # High card: Ace, King, Queen, Jack, 9
+        self.assertEqual(hand1.compare(hand2), 0)
+
+    def test_compare_high_card_higher_wins(self):
+        hand1 = Hand("ahkdqcjd8s")  # High card: Ace, King, Queen, Jack, 8
+        hand2 = Hand("ahkdqcjd9s")  # High card: Ace, King, Queen, Jack, 9
+        self.assertEqual(hand1.compare(hand2), -1)
+
+    # One Pair Tests
+    def test_compare_one_pair_equal(self):
+        hand1 = Hand("3s3d5h7c9d")  # Pair of 3s
+        hand2 = Hand("3h3c5d7s9c")  # Pair of 3s
+        self.assertEqual(hand1.compare(hand2), 0)
+
+    def test_compare_one_pair_higher_wins(self):
+        hand1 = Hand("3s3d5h7c9d")  # Pair of 3s
+        hand2 = Hand("4h4c5d7s9c")  # Pair of 4s
+        self.assertEqual(hand1.compare(hand2), -1)
+
+    # Two Pair Tests
+    def test_compare_two_pair_equal(self):
+        hand1 = Hand("3s3d5h5c9d")  # Two pair: 3s and 5s
+        hand2 = Hand("3h3c5s5d9c")  # Two pair: 3s and 5s
+        self.assertEqual(hand1.compare(hand2), 0)
+
+    def test_compare_two_pair_higher_wins(self):
+        hand1 = Hand("3s3d5h5c9d")  # Two pair: 3s and 5s
+        hand2 = Hand("4h4c5s5d9c")  # Two pair: 4s and 5s
+        self.assertEqual(hand1.compare(hand2), -1)
+
+    # Three of a Kind Tests
+    def test_compare_three_of_a_kind_equal(self):
+        hand1 = Hand("8s8d8h3c2d")  # Three of a kind: 8s
+        hand2 = Hand("8c8h8d4s2c")  # Three of a kind: 8s
+        self.assertEqual(hand1.compare(hand2), 0)
+
+    def test_compare_three_of_a_kind_higher_wins(self):
+        hand1 = Hand("7s7d7h3c2d")  # Three of a kind: 7s
+        hand2 = Hand("8c8h8d4s2c")  # Three of a kind: 8s
+        self.assertEqual(hand1.compare(hand2), -1)
+
+    # Straight Tests
+    def test_compare_straight_equal(self):
+        hand1 = Hand("5s6d7h8c9d")  # 9-high straight
+        hand2 = Hand("5h6c7d8s9c")  # 9-high straight
+        self.assertEqual(hand1.compare(hand2), 0)
+
+    def test_compare_straight_higher_wins(self):
+        hand1 = Hand("5s6d7h8c9d")  # 9-high straight
+        hand2 = Hand("6h7c8d9sTc")  # 10-high straight
+        self.assertEqual(hand1.compare(hand2), -1)
+
+    # Flush Tests
+    def test_compare_flush_equal(self):
+        hand1 = Hand("2s4s6s8sTs")  # Flush with Ts high
+        hand2 = Hand("2d4d6d8dTd")  # Flush with Ts high
+        self.assertEqual(hand1.compare(hand2), 0)
+
+    def test_compare_flush_higher_wins(self):
+        hand1 = Hand("2s4s6s8s9s")  # Flush with 9s high
+        hand2 = Hand("2d4d6d8dTs")  # Flush with Ts high
+        self.assertEqual(hand1.compare(hand2), -1)
+
+    # Full House Tests
+    def test_compare_full_house_equal(self):
+        hand1 = Hand("3s3c3h5s5d")  # Full House: 3s over 5s
+        hand2 = Hand("3d3h3c5d5h")  # Full House: 3s over 5s
+        self.assertEqual(hand1.compare(hand2), 0)
+
+    def test_compare_full_house_higher_wins(self):
+        hand1 = Hand("3s3c3h5s5d")  # Full House: 3s over 5s
+        hand2 = Hand("4d4h4c5d5h")  # Full House: 4s over 5s
+        self.assertEqual(hand1.compare(hand2), -1)
+
+    # Four of a Kind Tests
+    def test_compare_four_of_a_kind_equal(self):
+        hand1 = Hand("4s4hjs4c4d")  # Four of a kind: 4s
+        hand2 = Hand("4h4d4c4s7s")  # Four of a kind: 4s
+        self.assertEqual(hand1.compare(hand2), 0)
+
+    def test_compare_four_of_a_kind_higher_wins(self):
+        hand1 = Hand("3s3h3d3c5h")  # Four of a kind: 3s
+        hand2 = Hand("4h4d4c4s7s")  # Four of a kind: 4s
+        self.assertEqual(hand1.compare(hand2), -1)
+
+    # Straight Flush Tests
+    def test_compare_straight_flush_equal(self):
+        hand1 = Hand("5s6s7s8s9s")  # 9-high straight flush
+        hand2 = Hand("5h6h7h8h9h")  # 9-high straight flush
+        self.assertEqual(hand1.compare(hand2), 0)
+
+    def test_compare_straight_flush_higher_wins(self):
+        hand1 = Hand("5s6s7s8s9s")  # 9-high straight flush
+        hand2 = Hand("6h7h8h9hTh")  # 10-high straight flush
+        self.assertEqual(hand1.compare(hand2), -1)
+
+
+if __name__ == "__main__":
     unittest.main()
